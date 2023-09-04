ae992c9069606824129742b79e18e3ca1a22c6f4
Titus K. Cheserem <cheseremtitus24@gmail.com>
2023-09-04 09:58:51 +0300

    modified:   utils/db.js

--- utils/db.js
+++ utils/db.js
@@ -8,7 +8,7 @@ class DBClient {
    {"redis":true,"db":true}
    */
   constructor() {
-    const host = process.env.DB_HOST || '192.168.43.110';
+    const host = process.env.DB_HOST || 'localhost';
     const port = process.env.DB_PORT || 27017;
     const database = process.env.DB_DATABASE || 'files_manager';
     const url = `mongodb://${host}:${port}/${database}`;
